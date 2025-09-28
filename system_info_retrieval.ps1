69d3fdfc64a0fca9c44e6e0dfdf1dcdba2d9450a
Evrixol <56212925+Evrixol@users.noreply.github.com>
2025-09-28 13:36:36 -0600

    Finished uncompleted function "Write-Breaker" which will write eighty dashes and then a carriage return and line feed to output when called.

--- system_info_retrieval.ps1
+++ system_info_retrieval.ps1
@@ -67,31 +67,36 @@
 # Basic function to type out eighty (80) dashes ("-") and then a carriage return and line-feed, this
 # will act as a basic seperator for the different "Common Information Module" (CIM) class instance
 # requests.
-function Write
+function Write-Breaker {
+	for($i = 0; $i -LT 80; $i++) {
+		Write-Output "-"
+	}
+	Write-Output "`r`n"
+}
 
 # Basic Input / Output System (BIOS)
 Get-CimInstance -ClassName CIM_BIOSElement | Select-Object -Property BIOSVersion, Manufacturer,
 	Version, InstallDate, ReleaseDate, BuildNumber
 
-Write-Output "`r`n"
+Write-Breaker
 
 # Central Processing Unit (CPU)
 Get-CimInstance -ClassName CIM_Processor | Select-Object -Property DeviceID, LoadPercentage, Name,
 	Description, CurrentClockSpeed, Manufacturer, ThreadCount
 
-Write-Output "`r`n"
+Write-Breaker
 
 # Physical Memory (RAM)
 Get-CimInstance -ClassName Win32_PhysicalMemory | Select-Object -Property Caption, BankLabel,
 	Capacity, Speed
 
-Write-Output "`r`n"
+Write-Breaker
 
 # Storage Media (Storage Disks)
 Get-CimInstance -ClassName CIM_LogicalDisk | Select-Object -Property VolumeName, DeviceID,
 	FileSystem, Compressed, Size,	FreeSpace
 
-Write-Output "`r`n"
+Write-Breaker
 
 # Graphics Processing Unit (GPU)
 Get-CimInstance -ClassName CIM_VideoController | Select-Object -Property Name, DeviceID, AdapterRAM,
